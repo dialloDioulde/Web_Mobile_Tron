ff5878e69537e80ce24f31041fdd16b9de49a958
DIALLO Diouldé <diouldmariia@hotmail.fr>
2020-11-21 18:21:54 +0100

    server.js

--- www/js/server.js
+++ www/js/server.js
@@ -1,8 +1,27 @@
-var net = require('net');
-
+/*var net = require('net');
 var server = net.createServer(function (socket){
         socket.on('data', function(data){
         socket.write('server reply: ' + data);
     });
 });
-server.listen(8888);
+server.listen(8888);*/
+
+const WebSocket = require("ws");
+const server = new WebSocket.Server({
+  port: 8080,
+});
+
+let sockets = [];
+server.on("connection", function (socket) {
+  sockets.push(socket);
+
+  // When you receive a message, send that message to every socket.
+  socket.on("message", function (msg) {
+    sockets.forEach((s) => s.send(msg + " is server !"));
+  });
+
+  // When a socket closes, or disconnects, remove it from the array.
+  socket.on("close", function () {
+    sockets = sockets.filter((s) => s !== socket);
+  });
+});
